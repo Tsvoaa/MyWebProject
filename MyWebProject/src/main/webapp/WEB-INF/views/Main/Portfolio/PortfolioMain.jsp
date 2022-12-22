ee0e876b788ab96d632218b47b111c00bfda328d
Tsvoaa <gugudndn@naver.com>
2022-12-22 22:31:11 +0900

    20221222Update PortfolioMenu

--- MyWebProject/src/main/webapp/WEB-INF/views/Main/Portfolio/PortfolioMain.jsp
+++ MyWebProject/src/main/webapp/WEB-INF/views/Main/Portfolio/PortfolioMain.jsp
@@ -9,7 +9,7 @@
 	integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
 	crossorigin="anonymous">
 <link rel="stylesheet" href = "/my/resources/css/PortfolioView.css?ver=4" />
-<script defer src="/my/resources/js/PortfolioView.js?ver=1" ></script>
+<script defer src="/my/resources/js/PortfolioView.js?ver=2" ></script>
 </head>
 <body>
 	<div class="PortfolioContainer">
@@ -19,9 +19,9 @@
   				<li class="list-group-item subjectMenu">이수과목</li>
   				<li class="list-group-item projectMenu">프로젝트
   					<ul class="PortfolioList">
-  						<li class="list-group-item HomepeasyMenu">HomePEasy</li>
-  						<li class="list-group-item studentMenu">학생관리</li>
-  						<li class="list-group-item imageMenu">이미지번역</li>
+  						<li class="list-group-item homepeasyMenu hidden">HomePEasy</li>
+  						<li class="list-group-item studentMenu hidden">학생관리</li>
+  						<li class="list-group-item imageMenu hidden">이미지번역</li>
 					</ul>
   				</li>
   				<li class="list-group-item ohterMenu">기타</li>
@@ -36,6 +36,15 @@
 			</div>
 			<div class="project hidden">
 				333333
+				<div class="homepeasy hidden">
+					555555
+				</div>
+				<div class="student hidden">
+					666666
+				</div>
+				<div class="image hidden">
+					777777
+				</div>
 			</div>
 			<div class="ohter hidden">
 				444444
--- MyWebProject/src/main/webapp/resources/js/PortfolioView.js
+++ MyWebProject/src/main/webapp/resources/js/PortfolioView.js
@@ -1,26 +1,168 @@
-
+//좌측 메뉴
 const personalMenu = document.querySelector(".personalMenu");
 const subjectMenu = document.querySelector(".subjectMenu");
+const projectMenu = document.querySelector(".projectMenu");
+const ohterMenu = document.querySelector(".ohterMenu");
 
+//프로젝트 하위 메뉴
+const homepeasyMenu = document.querySelector(".homepeasyMenu");
+const studentMenu = document.querySelector(".studentMenu");
+const imageMenu = document.querySelector(".imageMenu");
+
+//메인 페이지
 const personal = document.querySelector(".personal");
 const subject = document.querySelector(".subject");
 const project = document.querySelector(".project");
 const ohter = document.querySelector(".ohter");
 
+// 프로젝트 하위 메인 페이지
+const homepeasy = document.querySelector(".homepeasy");
+const student = document.querySelector(".student");
+const image = document.querySelector(".image");
 
 personalMenu.addEventListener("click", () => {
 	subject.classList.add("hidden");
-	personal.classList.remove("hidden");
+	project.classList.add("hidden");
+	ohter.classList.add("hidden");
+	
+	homepeasyMenu.classList.add("hidden");
+	studentMenu.classList.add("hidden");
+	imageMenu.classList.add("hidden");
+	
+	homepeasy.classList.add("hidden");
+	student.classList.add("hidden");
+	image.classList.add("hidden");
 	
 	subjectMenu.classList.remove("active");
+	projectMenu.classList.remove("active");
+	ohterMenu.classList.remove("active");
+	
+	homepeasyMenu.classList.remove("active");
+	studentMenu.classList.remove("active");
+	imageMenu.classList.remove("active");
+	
+	
+	personal.classList.remove("hidden");
 	personalMenu.classList.add("active");
+	
 	
 });
 
 subjectMenu.addEventListener("click", () => {
 	personal.classList.add("hidden");
-	subject.classList.remove("hidden");
+	project.classList.add("hidden");
+	ohter.classList.add("hidden");
 	
-	subjectMenu.classList.add("active");
+	homepeasyMenu.classList.add("hidden");
+	studentMenu.classList.add("hidden");
+	imageMenu.classList.add("hidden");
+	
+	homepeasy.classList.add("hidden");
+	student.classList.add("hidden");
+	image.classList.add("hidden");
+	
 	personalMenu.classList.remove("active");
-})
+	projectMenu.classList.remove("active");
+	ohterMenu.classList.remove("active");
+	
+	homepeasyMenu.classList.remove("active");
+	studentMenu.classList.remove("active");
+	imageMenu.classList.remove("active");
+	
+	subject.classList.remove("hidden");
+	subjectMenu.classList.add("active");
+	
+	
+});
+
+projectMenu.addEventListener("click", () => {
+	personal.classList.add("hidden");
+	subject.classList.add("hidden");
+	ohter.classList.add("hidden");
+	
+	personalMenu.classList.remove("active");
+	subjectMenu.classList.remove("active");
+	ohterMenu.classList.remove("active");
+	
+	homepeasyMenu.classList.remove("active");
+	studentMenu.classList.remove("active");
+	imageMenu.classList.remove("active");
+	
+	project.classList.remove("hidden");
+	//projectMenu.classList.add("active");
+	
+	homepeasyMenu.classList.remove("hidden");
+	studentMenu.classList.remove("hidden");
+	imageMenu.classList.remove("hidden");
+	
+});
+
+ohterMenu.addEventListener("click", () => {
+	personal.classList.add("hidden");
+	subject.classList.add("hidden");
+	project.classList.add("hidden");
+	
+	homepeasy.classList.add("hidden");
+	student.classList.add("hidden");
+	image.classList.add("hidden");
+	
+	homepeasyMenu.classList.add("hidden");
+	studentMenu.classList.add("hidden");
+	imageMenu.classList.add("hidden");
+	
+	
+	personalMenu.classList.remove("active");
+	subjectMenu.classList.remove("active");
+	projectMenu.classList.remove("active");
+	
+	homepeasyMenu.classList.remove("active");
+	studentMenu.classList.remove("active");
+	imageMenu.classList.remove("active");
+	
+	ohter.classList.remove("hidden");
+	ohterMenu.classList.add("active");
+	
+	
+});
+
+homepeasyMenu.addEventListener("click", () => {
+	
+	student.classList.add("hidden");
+	image.classList.add("hidden");
+	
+	studentMenu.classList.remove("active");
+	imageMenu.classList.remove("active");
+	
+	homepeasyMenu.classList.add("active");
+	homepeasy.classList.remove("hidden");
+	
+	
+});
+
+studentMenu.addEventListener("click", () => {
+	
+	homepeasy.classList.add("hidden");
+	image.classList.add("hidden");
+	
+	homepeasyMenu.classList.remove("active");
+	imageMenu.classList.remove("active");
+	
+	studentMenu.classList.add("active");
+	student.classList.remove("hidden");
+	
+	
+});
+
+imageMenu.addEventListener("click", () => {
+	
+	homepeasy.classList.add("hidden");
+	student.classList.add("hidden");
+	
+	studentMenu.classList.remove("active");
+	homepeasyMenu.classList.remove("active");
+	
+	
+	imageMenu.classList.add("active");
+	image.classList.remove("hidden");
+	
+});
